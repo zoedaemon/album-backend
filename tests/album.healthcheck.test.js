951516c03c687f7b2797dba80b46d0f6ec1dff4e
roy adventus <lindroy34@gmail.com>
2020-08-14 02:19:42 +0700

    simple health check testcase - jest

---
+++ tests/album.healthcheck.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+describe('GET /health', () => {
+    
+    it('should OK', async () => {
+        const res = await request(app)
+            .get('/health')
+            .send()
+        expect(res.statusCode).toEqual(200)
+    })
+})
