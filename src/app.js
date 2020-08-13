b1cfa9b42e8e60d72c058d2485345c50680fa5bb
roy adventus <lindroy34@gmail.com>
2020-08-14 02:52:04 +0700

    use helmet & cors for security reason

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express')
-const port = process.env.PORT
+const helmet = require('helmet');
+const cors = require('cors');
 const albumRouter = require('./routers/album')
+const port = process.env.PORT
 
 const app = express()
 
 app.use(express.json())
 app.use(albumRouter)
+app.use(helmet());
 
 if (process.env && process.env.NODE_ENV != 'test') {
+    
+    //TODO: use cors by domain name in production mode - 
+    //      register DOMAIN variable in environment variable
+    app.use(cors());
+    app.options('*', cors());
+    
     app.listen(port, () => {
         console.log(`Server running on port ${port}`)
     })
