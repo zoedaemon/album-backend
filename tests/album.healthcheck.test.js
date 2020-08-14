e67b18efd2aac0dee6f6f8c41d1289e978038276
Unknown <unknown@example.com>
2020-08-15 01:01:10 +0700

    repair linter styling - use standardjs style

--- tests/album.healthcheck.test.js
+++ tests/album.healthcheck.test.js
@@ -2,11 +2,10 @@ const request = require('supertest')
 const app = require('../src/app')
 
 describe('GET /health', () => {
-    
-    it('should OK', async () => {
-        const res = await request(app)
-            .get('/health')
-            .send()
-        expect(res.statusCode).toEqual(200)
-    })
+  it('should OK', async () => {
+    const res = await request(app)
+      .get('/health')
+      .send()
+    expect(res.statusCode).toEqual(200)
+  })
 })
