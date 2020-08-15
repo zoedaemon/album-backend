0ae733d71ac1995a87a63e3cd34e0409e75f1614
Roy Adventus <lindroy34@gmail.com>
2020-08-16 06:06:14 +0700

    create new migration and models based on sequelize package
    NOTE: if wanna doit manual migration just execute "npx sequelize-cli db:migrate"

---
+++ src/migrations/20200815224111-create-album.js
@@ -0,0 +1,41 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Albums', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      // album allowed to null so this would be put in parent directory 
+      album: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      raw: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Albums')
+  }
+}
