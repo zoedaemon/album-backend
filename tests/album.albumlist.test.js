b4b6354106a7fc84ae39cc8de75f6c834d9d050c
Roy Adventus <lindroy34@gmail.com>
2020-08-17 06:14:26 +0700

    better test with pretest "migrate up" and posttest "migrate down"

--- tests/album.albumlist.test.js
+++ tests/album.albumlist.test.js
@@ -1,8 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const config = require('../src/config/index')
 const db = require('../src/models/index')
 
-describe('GET /health', () => {
+describe('POST /list', () => {
+
+  // beforeEach(async () => {
+  //   await request(app)
+  //     .put(config.api.prefix)
+  //     .send()
+  //   return true;
+  // })
+
   /* if still error : "A worker process has failed to exit gracefully and has been force exited.
   This is likely caused by tests leaking due to improper teardown.
   Try running with --runInBand --detectOpenHandles to find leaks."
@@ -10,9 +19,9 @@ describe('GET /health', () => {
   */
   afterAll(() => db.sequelize.close())
 
-  it('should OK', async () => {
+  it('should OK and return list data', async () => {
     const res = await request(app)
-      .get('/health')
+      .post(config.api.prefix + '/list')
       .send()
     expect(res.statusCode).toEqual(200)
   })
