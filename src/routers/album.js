1afa403734c51420e999f341cfd7f45e304ba8c8
Roy Adventus <lindroy34@gmail.com>
2020-08-18 03:48:06 +0700

    parallel upload and get correct result with Promise; async create album subdir (func createDirIfNotExist)

--- src/routers/album.js
+++ src/routers/album.js
@@ -15,16 +15,28 @@ const fs = require('fs').promises
 const formidable = require('formidable')
 const path = require('path')
 
-/* eslint no-useless-catch: "error" */
+// upload file asnycronously
 async function uploader (srcPath, dstPath) {
   try {
     const srcFile = await fs.readFile(srcPath)
     await fs.writeFile(dstPath, srcFile)
+    return '/' + dstPath
   } catch (error) {
     console.log('error : ' + error)
-    return false
+    return null
   }
-  return true
+}
+
+// create directory asyncronously
+async function createDirIfNotExist (path) {
+  try {
+    const stats = await fs.lstat(path)
+    if (stats.isDirectory()) {
+      ;// do nothing if is correct dir
+    }
+  } catch {
+    await fs.mkdir(path)
+  }
 }
 
 // DONE : 503 for WARN, e.g. high CPU, high memory usage
@@ -66,39 +78,58 @@ router.post(config.api.prefix + '/list', async (req, res) => {
 // TODO: better to put in controllers/logics/helpers modules
 router.put(config.api.prefix, async (req, res) => {
   try {
+
+    // set multipart
     const formPost = formidable({ multiples: true })
-    await formPost.parse(req, async (err, fields, files, next) => {
-      if (err) {
-        next(err)
-        return
-      }
 
-      let albumName = ''
-      if (fields.album) {
-        albumName = fields.album.toLowerCase()
-      }
-      // TODO: if saveToPath not exist create folder
-      const saveToPath = path.join(config.albumPath, albumName) + '/'
+    // create promise for corectly parse all data needed to upload
+    const results = await new Promise((resolve, reject) => {
+      formPost.parse(req, async (err, fields, files) => {
+        if (err) {
+          reject(err)
+          return
+        }
 
-      if (files.documents && files.documents.length > 0) {
-        files.documents.map(async (srcFile) => {
-          // TODO: clean up malicious file name (slash, dot, etc)
-          const dstPath = saveToPath + srcFile.name
-          console.log(srcFile.name + ':' + srcFile.type)
+        let albumName = ''
+        if (fields.album) {
+          albumName = fields.album.toLowerCase()
+        }
 
-          // upload image in documents
-          const results = await uploader(srcFile.path, dstPath)
-          if (results) {
-            const response = { message: 'OK' }
-            res.status(200).json(response)
-          } else {
-            // TODO: clean up last uploaded images
-            const response = { message: 'ERROR' }
-            res.status(500).json(response)
+        // DONE: if saveToPath not exist create directory
+        const saveToPath = path.join(config.albumPath, albumName) + '/'
+        await createDirIfNotExist(saveToPath)
+
+        if (files.documents && files.documents.length > 0) {
+          let resultRaw = []
+          // doing parallel upload
+          const resultsPromises = files.documents.map(async (srcFile) => {
+            // TODO: clean up malicious file name (slash, dots, etc)
+            const dstPath = saveToPath + srcFile.name
+            console.log(srcFile.name + ':' + srcFile.type)
+
+            // upload image in documents to dstPath
+            return { path: await uploader(srcFile.path, dstPath) }
+          })
+
+          // log & assign sequentially
+          for (const resultsPromise of resultsPromises) {
+            const r = await resultsPromise
+            console.log(r)
+            resultRaw.push(r)
           }
-        })
-      }
+          resolve(resultRaw)
+        }
+      })
     })
+
+    // check results
+    if (results.length > 0) {
+      const response = { message: 'OK', data: results }
+      res.status(200).json(response)
+    } else {
+      const response = { message: 'ERROR' }
+      res.status(500).json(response)
+    }
   } catch (error) {
     console.log('error : ' + error)
     res.status(500).json({ message: 'ERROR' })
